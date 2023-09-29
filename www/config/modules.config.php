76950890ffe1e8b6b972b2f7aba75bc6be01f2c3
Kirill Vnukov <vnukov.k@introvert.bz>
2023-09-30 02:58:47 +0300

    Install the modules

--- www/config/modules.config.php
+++ www/config/modules.config.php
@@ -6,6 +6,14 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Navigation',
+    'Laminas\I18n',
+    'Laminas\Session',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Db',
     'Laminas\Router',
     'Laminas\Validator',
     'Application',
