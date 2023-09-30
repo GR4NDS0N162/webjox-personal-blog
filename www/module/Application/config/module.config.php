f319e863e4bfa7fc8b5e07909bf0544e156c63bc
Kirill Vnukov <vnukov.k@introvert.bz>
2023-09-30 05:33:23 +0300

    Implement the dependency on the registration form

--- www/module/Application/config/module.config.php
+++ www/module/Application/config/module.config.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Application;
 
 return [
-    'router'       => include __DIR__ . '/configs/router.php',
-    'controllers'  => include __DIR__ . '/configs/controllers.php',
-    'view_manager' => include __DIR__ . '/configs/view_manager.php',
+    'router'        => include __DIR__ . '/configs/router.php',
+    'form_elements' => include __DIR__ . '/configs/form_elements.php',
+    'controllers'   => include __DIR__ . '/configs/controllers.php',
+    'view_manager'  => include __DIR__ . '/configs/view_manager.php',
 ];
--- www/module/Application/src/Factory/Controller/IndexControllerFactory.php
+++ www/module/Application/src/Factory/Controller/IndexControllerFactory.php
@@ -5,13 +5,23 @@ declare(strict_types=1);
 namespace Application\Factory\Controller;
 
 use Application\Controller\IndexController;
+use Application\Form\Index\SignUpForm;
 use Interop\Container\ContainerInterface;
+use Laminas\Form\FormElementManager;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class IndexControllerFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): object
     {
-        return new IndexController();
+        /** @var ContainerInterface $formManager */
+        $formManager = $container->get(FormElementManager::class);
+
+        /** @var SignUpForm $signUpForm */
+        $signUpForm = $formManager->get(SignUpForm::class);
+
+        return new IndexController(
+            $signUpForm,
+        );
     }
 }
