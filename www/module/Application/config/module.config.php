6231aef5cb205711cad0cce76a598051df06368c
Kirill Vnukov <vnukov.k@introvert.bz>
2023-09-30 16:01:51 +0300

    Add view helper configurations

--- www/module/Application/config/module.config.php
+++ www/module/Application/config/module.config.php
@@ -9,5 +9,6 @@ return [
     'router'          => include __DIR__ . '/configs/router.php',
     'form_elements'   => include __DIR__ . '/configs/form_elements.php',
     'controllers'     => include __DIR__ . '/configs/controllers.php',
+    'view_helpers'    => include __DIR__ . '/configs/view_helpers.php',
     'view_manager'    => include __DIR__ . '/configs/view_manager.php',
 ];
