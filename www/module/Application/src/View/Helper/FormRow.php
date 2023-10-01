c7ef5f457ffc9753d459e8d197cb4bff803516a8
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-01 23:47:13 +0300

    Add a partial script for FormRow

--- www/module/Application/src/View/Helper/FormRow.php
+++ www/module/Application/src/View/Helper/FormRow.php
@@ -6,4 +6,8 @@ namespace Application\View\Helper;
 
 class FormRow extends \Laminas\Form\View\Helper\FormRow
 {
+    public function __construct()
+    {
+        $this->setPartial('partial/form-row');
+    }
 }
