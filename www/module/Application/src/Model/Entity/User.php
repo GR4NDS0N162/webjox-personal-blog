a5656d78426886d2591003c17e4f7df0e4d99027
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-02 19:24:05 +0300

    Add getters and setters

--- www/module/Application/src/Model/Entity/User.php
+++ www/module/Application/src/Model/Entity/User.php
@@ -13,6 +13,58 @@ use Laminas\InputFilter\InputFilterInterface;
 
 class User implements InputFilterAwareInterface, HydratorAwareInterface
 {
+    private ?int $id;
+
+    private ?string $email;
+
+    private ?string $password;
+
+    private ?int $roleId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): User
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): User
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getRoleId(): ?int
+    {
+        return $this->roleId;
+    }
+
+    public function setRoleId(?int $roleId): User
+    {
+        $this->roleId = $roleId;
+        return $this;
+    }
+
     private InputFilterInterface $inputFilter;
 
     private HydratorInterface $hydrator;
