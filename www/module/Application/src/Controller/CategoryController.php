de7a2812fb3963b88af40c644a3fd6c268112e6c
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-03 13:33:34 +0300

    Edit class

--- www/module/Application/src/Controller/CategoryController.php
+++ www/module/Application/src/Controller/CategoryController.php
@@ -7,11 +7,13 @@ namespace Application\Controller;
 use Laminas\Form\FormElementManager;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\ServiceManager\ServiceManager;
+use Laminas\Session\Container as SessionContainer;
 
 class CategoryController extends AbstractActionController
 {
     public function __construct(
         private ServiceManager $serviceManager,
         private FormElementManager $formElementManager,
+        private SessionContainer $sessionContainer,
     ) {}
 }
