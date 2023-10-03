4e617ec098dc1a87079e77f0a0ba7b4f529d3c3c
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-03 11:57:12 +0300

    Implement HydratorAwareInterface

--- www/module/Application/src/Model/Entity/User.php
+++ www/module/Application/src/Model/Entity/User.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Application\Model\Entity;
 
-class User
+use Laminas\Hydrator\ClassMethodsHydrator;
+use Laminas\Hydrator\HydratorAwareInterface;
+use Laminas\Hydrator\HydratorInterface;
+use Laminas\Hydrator\Strategy\NullableStrategy;
+use Laminas\Hydrator\Strategy\ScalarTypeStrategy;
+
+class User implements HydratorAwareInterface
 {
+    private HydratorInterface $hydrator;
+
     public function __construct(
         private ?int $id = null,
         private ?string $email = null,
@@ -55,5 +63,32 @@ class User
     {
         $this->roleId = $roleId;
         return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setHydrator(HydratorInterface $hydrator): void
+    {
+        $this->hydrator = $hydrator;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getHydrator(): ?HydratorInterface
+    {
+        if (isset($this->hydrator)) {
+            return $this->hydrator;
+        }
+
+        $this->hydrator = new ClassMethodsHydrator(true, true);
+
+        $this->hydrator->addStrategy('id', new NullableStrategy(ScalarTypeStrategy::createToInt(), true));
+        $this->hydrator->addStrategy('email', new NullableStrategy(ScalarTypeStrategy::createToString(), true));
+        $this->hydrator->addStrategy('password', new NullableStrategy(ScalarTypeStrategy::createToString(), true));
+        $this->hydrator->addStrategy('role_id', new NullableStrategy(ScalarTypeStrategy::createToInt(), true));
+
+        return $this->hydrator;
     }
 }
