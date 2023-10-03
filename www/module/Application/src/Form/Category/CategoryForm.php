63c6828c8cf0dd79ac6446ca9eb68e8253b7078d
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-03 14:01:48 +0300

    Add CategoryForm

---
+++ www/module/Application/src/Form/Category/CategoryForm.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Form\Category;
+
+use Application\View\Helper\FormRow;
+use Laminas\Form\Element\Button;
+use Laminas\Form\Element\Collection;
+use Laminas\Form\Form;
+
+class CategoryForm extends Form
+{
+    /**
+     * @inheritDoc
+     */
+    public function init(): void
+    {
+        parent::init();
+
+        $this->setAttribute('class', 'row g-3');
+
+        $this->add([
+            'name'       => 'list',
+            'type'       => Collection::class,
+            'attributes' => [
+                'class' => 'row g-3',
+            ],
+            'options'    => [
+                'count'                  => 3,
+                'allow_add'              => true,
+                'allow_remove'           => true,
+                'should_create_template' => true,
+                'template_placeholder'   => '__index__',
+                'target_element'         => [
+                    'type' => CategoryFieldset::class,
+                ],
+                FormRow::WRAPPER_CLASS   => 'col-12',
+            ],
+        ]);
+
+        $this->add([
+            'name'       => 'submit',
+            'type'       => Button::class,
+            'attributes' => [
+                'type'  => 'submit',
+                'class' => 'btn btn-lg btn-outline-success w-100',
+                'id'    => 'category_form-submit',
+            ],
+            'options'    => [
+                'label'                => 'Save',
+                FormRow::WRAPPER_CLASS => 'col-12',
+            ],
+        ], ['priority' => -10 ** 9]);
+    }
+}
