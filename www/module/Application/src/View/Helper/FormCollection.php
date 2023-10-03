afbbbc5a5c0674eb90a1b972957c05779f8bd961
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-03 14:19:44 +0300

    Override render() method

--- www/module/Application/src/View/Helper/FormCollection.php
+++ www/module/Application/src/View/Helper/FormCollection.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace Application\View\Helper;
 
+use Laminas\Form\ElementInterface;
+
 class FormCollection extends \Laminas\Form\View\Helper\FormCollection
 {
+    /**
+     * @inheritDoc
+     */
+    public function render(ElementInterface $element): string
+    {
+        $markup = parent::render($element);
+
+        $wrapperClass = $element->getOption(FormRow::WRAPPER_CLASS);
+        if (!empty($wrapperClass)) {
+            $markup = sprintf('<div class="%1$s">%2$s</div>', $wrapperClass, $markup);
+        }
+
+        return $markup;
+    }
 }
