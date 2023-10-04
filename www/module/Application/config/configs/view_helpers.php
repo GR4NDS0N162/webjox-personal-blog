936970fd30b73e118cf4185184678369519bfbbb
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-04 17:58:09 +0300

    Redefine view helper

--- www/module/Application/config/configs/view_helpers.php
+++ www/module/Application/config/configs/view_helpers.php
@@ -5,19 +5,24 @@ declare(strict_types=1);
 namespace Application;
 
 use Application\View\Helper\FormCollection;
+use Application\View\Helper\FormMultiCheckbox;
 use Application\View\Helper\FormRow;
 use Laminas\ServiceManager\Factory\InvokableFactory;
 
 return [
     'aliases'   => [
-        'formcollection'  => FormCollection::class,
-        'form_collection' => FormCollection::class,
-        'formCollection'  => FormCollection::class,
-        'FormCollection'  => FormCollection::class,
-        'formrow'         => FormRow::class,
-        'form_row'        => FormRow::class,
-        'formRow'         => FormRow::class,
-        'FormRow'         => FormRow::class,
+        'formmulticheckbox'   => FormMultiCheckbox::class,
+        'form_multi_checkbox' => FormMultiCheckbox::class,
+        'formMultiCheckbox'   => FormMultiCheckbox::class,
+        'FormMultiCheckbox'   => FormMultiCheckbox::class,
+        'formcollection'      => FormCollection::class,
+        'form_collection'     => FormCollection::class,
+        'formCollection'      => FormCollection::class,
+        'FormCollection'      => FormCollection::class,
+        'formrow'             => FormRow::class,
+        'form_row'            => FormRow::class,
+        'formRow'             => FormRow::class,
+        'FormRow'             => FormRow::class,
     ],
     'factories' => [
         FormCollection::class => InvokableFactory::class,
