3642deb6fbd54fe68d059e3ba03dedcc0ffde91d
Kirill Vnukov <vnukov.k@introvert.bz>
2023-10-05 13:43:00 +0300

    Install modules

--- www/config/modules.config.php
+++ www/config/modules.config.php
@@ -6,6 +6,8 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Paginator\Adapter\LaminasDb',
+    'Laminas\Paginator',
     'Laminas\Di',
     'Laminas\Navigation',
     'Laminas\I18n',
